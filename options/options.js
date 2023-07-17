129966bf8dcb488f59953e070f32a3bc9f8e2e9e
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 03:24:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cb-app-demo-1-dev-102054",
+  url: "https://cb-app-demo-1-dev-102054.botics.co",
+  api: "https://cb-app-demo-1-dev-102054.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
